5d27805d21b533c01d0772f6761d2371c46f5c19
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2018-03-16 13:35:27 +0200

    Can't use bower_componets as source folder, bower doesn't installs dependencies in parallel

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
-'use strict';
+const gulp = require('gulp'),
+    sass = require('gulp-sass');
  
-const gulp = require('gulp');
-const sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task("js", () => {
+    let sourceFiles = [ 
+        'bower_components/simplemde/dist/simplemde.min.js'
+    ];
+    let destination = "."
+
+    return gulp.src(sourceFiles)
+        .pipe(gulp.dest(destination));
 });
  
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task("build", ["sass"]);
+gulp.task("build", ["sass", "js"]);
 gulp.task("default", ["build"]);
