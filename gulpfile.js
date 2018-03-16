2545a9efd40557f5126b56cb268fb9236014531c
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2018-03-16 14:50:51 +0200

    Inline the css styles in html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp'),
-    sass = require('gulp-sass');
- 
+    copy = require("gulp-copy"),
+    sass = require('gulp-sass'),
+    inline = require("gulp-inline"),
+    minifyCss = require("gulp-minify-css");
+
+const destination = "./dist"
+
 gulp.task('sass', () => {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -11,9 +16,18 @@ gulp.task("js", () => {
     let sourceFiles = [ 
         'bower_components/simplemde/dist/simplemde.min.js'
     ];
-    let destination = "."
 
     return gulp.src(sourceFiles)
+        .pipe(copy(destination, {prefix: 3}))
+        .pipe(gulp.dest(destination));
+});
+
+gulp.task("inline", ["sass"], () => {
+    return gulp.src('src/wc-simplemde.html')
+        .pipe(inline({
+            css: [minifyCss],
+            ignore: ['../simplemde.min.js']
+        }))
         .pipe(gulp.dest(destination));
 });
  
@@ -21,5 +35,5 @@ gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task("build", ["sass", "js"]);
+gulp.task("build", ["js", "inline"]);
 gulp.task("default", ["build"]);
